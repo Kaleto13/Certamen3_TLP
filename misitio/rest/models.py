8999459de7566352b80d5d349b67bfc9bb8cc29b
Javier-Lucero <javier.lucero@usm.cl>
2023-11-27 22:18:50 -0300

    models admin superuser
    arreglé los modelos, cree superusuario y agregue los modelos al admin

--- misitio/rest/models.py
+++ misitio/rest/models.py
@@ -8,10 +8,10 @@ class Evento(models.Model):
     fecha_termino = models.DateTimeField()
     titulo = models.CharField(max_length=30)
     descripcion = models.CharField(max_length=100)
-    tipos = [('Vacaciones'),('Feriado'),('Suspensión de actividades'),('Suspensión de actividades PM'),('Período Lectivo'),('Suspensión de evaluaciones'),('Ceremonia'),('EDDA'),('Evaluación'),('Ayudantías'),('Hito Académico'),('Secretaría Académica'),('OAI')]  
-   # tipo = models.CharField(max_length=1,choices=tipos,default='Vacaciones')
-    segmentos = [('Comunidad USM'),('Estudiante'),('Profesor'),('Jefe de Carrera')]
-   # segmento = models.CharField(choices=segmentos,default='Comunidad USM')
+    tipos = [('1','Vacaciones'),('2','Feriado'),('3','Suspensión de actividades'),('4','Suspensión de actividades PM'),('5','Período Lectivo'),('6','Suspensión de evaluaciones'),('7','Ceremonia'),('8','EDDA'),('9','Evaluación'),('10','Ayudantías'),('11','Hito Académico'),('12','Secretaría Académica'),('13','OAI')]  
+    tipo = models.CharField(max_length=2,choices=tipos,default='Vacaciones')
+    segmentos = [('C','Comunidad USM'),('E','Estudiante'),('P','Profesor'),('J','Jefe de Carrera')]
+    segmento = models.CharField(max_length=1,choices=segmentos,default='Comunidad USM')
 
 
 # Create your models here.
